13d0fe1bda7a32e2e83d6d22279707359e42ee47
Fuad Musayev <fuadmusayev@Fuads-MacBook-Pro.local>
2023-11-21 21:53:40 +0400

    data-plane values updated to runtime version 1.4.0

--- files/database-init.sql
+++ files/database-init.sql
@@ -6,6 +6,7 @@ CREATE DATABASE iomete_iceberg_db;
 CREATE DATABASE iomete_metastore_db;
 CREATE DATABASE iomete_keycloak_db;
 CREATE DATABASE iomete_ranger_db;
+CREATE DATABASE iomete_control_plane_db;
 
 CREATE USER iomete_user@'%' IDENTIFIED BY 'iomete_pass';
 GRANT ALL PRIVILEGES ON iomete_core_db.* TO iomete_user@'%';
@@ -15,5 +16,6 @@ GRANT ALL PRIVILEGES ON iomete_iceberg_db.* TO iomete_user@'%';
 GRANT ALL PRIVILEGES ON iomete_metastore_db.* TO iomete_user@'%';
 GRANT ALL PRIVILEGES ON iomete_keycloak_db.* TO iomete_user@'%';
 GRANT ALL PRIVILEGES ON iomete_ranger_db.* TO iomete_user@'%';
+GRANT ALL PRIVILEGES ON iomete_control_plane_db.* TO iomete_user@'%';
 
 FLUSH PRIVILEGES;
